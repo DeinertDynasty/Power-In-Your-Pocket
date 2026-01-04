c3d4588773080edde787298ad32104d770ef1330
Phoenix Deinert <phoenix.deinert@gmail.com>
2026-01-04 13:34:55 -0500

    urgency slides and file for use

---
+++ src/img/urgencyImages.js
@@ -0,0 +1,138 @@
+// src/img/urgencyImages.js
+// Central image registry for Urgency Training
+// Images live in src/img/urgency/
+
+// Window anatomy & parts
+import windowAnatomyFullPic from "./urgency/window anatomy full pic.jpg";
+import jamb from "./urgency/jamb.webp";
+import sash from "./urgency/sash.jpg";
+import casing from "./urgency/casing.jpg";
+import windowSill from "./urgency/window_SILL.webp";
+import mullion from "./urgency/mullion.jpg";
+import lights from "./urgency/lights.jpg";
+import muntin from "./urgency/Muntin.jpg";
+
+// Window types
+import singleHungDoubleHung from "./urgency/single-hung and double-hung window.jpg";
+import slidingWindow from "./urgency/sliding window.jpg";
+import projectedWindows from "./urgency/projected windows.jpg";
+import casementWindows from "./urgency/casement_Windows.jpg";
+import awningWindow from "./urgency/awning window.jpg";
+import hopperWindow from "./urgency/hopperwindow.jpg";
+import fixedWindow from "./urgency/fixed window.jpg"; // only if exists
+import bowWindow from "./urgency/bow window.jpg";
+import bayWindow from "./urgency/bay window.jpg";
+import pivotWindow from "./urgency/Pivot.png";
+
+// Trim / framing
+import apron from "./urgency/apron.webp";
+import fascia from "./urgency/fascia.webp";
+import soffit from "./urgency/soffit.jpg";
+import rake from "./urgency/rake.png";
+import dripEdge from "./urgency/drip_edge.jpg";
+import flashing from "./urgency/flashing.jpg";
+import jChannel from "./urgency/j channel.jpg";
+import fChannel from "./urgency/f channel.jpg";
+import tChannel from "./urgency/t channel.png";
+
+// Roof anatomy
+import roofAnatomyPic from "./urgency/roof anatomy pic.jpg";
+import roofingSystemDiagram1 from "./urgency/roofing_anatomy_1.jpg";
+import roofingSystemDiagram2 from "./urgency/Roofing System Diagram 2.jpg";
+import roofLayersSimplified from "./urgency/roof layers simplified.webp";
+import ridge from "./urgency/ridge.jpg";
+import valley from "./urgency/valley.jpg";
+import gable from "./urgency/gable.jpg";
+import roofEave from "./urgency/roof_eve.jpg";
+import boxGutters from "./urgency/box gutters.jpg";
+
+// Damage / failures
+import mildSealFailure from "./urgency/mild Seal Failure.jpg";
+import severeSealFailure from "./urgency/severe seal failure.jpg";
+import bowing from "./urgency/bowing.jpg";
+import woodRot from "./urgency/wood rot.jpg";
+import dryVsWetRot from "./urgency/dry vs wet rot.jpg";
+import shingleDamage from "./urgency/shingle damage.jpg";
+import granuleLoss from "./urgency/granule_loss.jpg";
+import roofLeaksStains from "./urgency/roof_leaks_stains.jpg";
+import exposedFiberglass from "./urgency/exposed fiberglass.jpg";
+
+// Siding
+import sidingAnatomy from "./urgency/siding anatomy.jpg";
+import sidingGutterAnatomy from "./urgency/siding:gutter anatomy.jpg";
+import sidingFace from "./urgency/siding Face.jpg";
+import boardAndBattenSiding from "./urgency/board and batten siding.jpg";
+import typesOfSiding from "./urgency/types of siding.jpg";
+
+// Doors
+import doorAnatomy from "./urgency/door anatomy.jpg";
+
+// Export lookup table
+const urgencyImages = {
+  // Window anatomy & parts
+  windowAnatomyFullPic,
+  jamb,
+  sash,
+  casing,
+  windowSill,
+  mullion,
+  lights,
+  muntin,
+
+  // Window types
+  singleHungDoubleHung,
+  slidingWindow,
+  projectedWindows,
+  casementWindows,
+  awningWindow,
+  hopperWindow,
+  fixedWindow,
+  bowWindow,
+  bayWindow,
+  pivotWindow,
+
+  // Trim / framing
+  apron,
+  fascia,
+  soffit,
+  rake,
+  dripEdge,
+  flashing,
+  jChannel,
+  fChannel,
+  tChannel,
+
+  // Roof anatomy
+  roofAnatomyPic,
+  roofingSystemDiagram1,
+  roofingSystemDiagram2,
+  roofLayersSimplified,
+  ridge,
+  valley,
+  gable,
+  roofEave,
+  boxGutters,
+
+  // Damage / failures
+  mildSealFailure,
+  severeSealFailure,
+  bowing,
+  woodRot,
+  dryVsWetRot,
+  shingleDamage,
+  granuleLoss,
+  roofLeaksStains,
+  exposedFiberglass,
+
+  // Siding
+  sidingAnatomy,
+  sidingGutterAnatomy,
+  sidingFace,
+  boardAndBattenSiding,
+  typesOfSiding,
+
+  // Doors
+  doorAnatomy,
+};
+
+export default urgencyImages;
